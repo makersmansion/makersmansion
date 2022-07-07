b37904fc4e5150f41465241e84dc9f1594af6a1a
Raz <razvan.tirboaca@gmail.com>
2022-07-07 11:18:37 +0200

    Build: Removes plugin page assets

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ const markdownItAnchor = require("markdown-it-anchor");
 const markdownItEmoji = require("markdown-it-emoji");
 const slugify = require("slugify");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pageAssetsPlugin = require('eleventy-plugin-page-assets');
 
 const filters = require('./utils/filters.js');
 
