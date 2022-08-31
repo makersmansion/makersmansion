7fd2182ef0752dd9f467c13bc4ba5c744a43c196
Raz <razvan.tirboaca@gmail.com>
2022-08-31 16:36:58 +0200

    Feat: Adds sans custom font and updates layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/**/*.{njk, md, html}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Figtree', defaultTheme.fontFamily.sans]
+      },
+      letterSpacing: {
+        tighter: '-.01em',
+        tight: '-.001em'
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
