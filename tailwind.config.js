be0b923e808e501b1bf28ec534ec69b3169c83a5
Raz <razvan.tirboaca@gmail.com>
2022-11-03 10:03:45 +0100

    Style: Let blockquote inherit font weight

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
             strong: {
               'font-weight': 'bold',
             },
+            blockquote: {
+              'font-weight': 'inherit',
+            }
           },
         },
         neutral: {
